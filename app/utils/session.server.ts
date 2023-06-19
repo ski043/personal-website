0beda2dc596a0b0177135a415c82c120dc60ff96
Jan Niklas Marzahl <janniklasmarzahl@gmail.com>
2023-06-19 12:55:22 +0200

    change

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -4,12 +4,14 @@ import { createThemeSessionResolver } from "remix-themes";
 const sessionStorage = createCookieSessionStorage({
   cookie: {
     name: "__remix-themes",
-    domain: process.env.NODE_ENV !== "development" ? "asdf" : null,
+    domain:
+      process.env.NODE_ENV == "production" &&
+      "https://personal-website-xi-lac.vercel.app/",
     path: "/",
     httpOnly: true,
     sameSites: "lax",
     secrets: ["secert"],
-    secure: process.env.NODE_ENV === "development" ? false : true,
+    secure: process.env.NODE_ENV === "development" && true,
   },
 });
 
