0137273d331fc22ce66e263e556f590a6ac43dae
Jan Niklas Marzahl <janniklasmarzahl@gmail.com>
2023-06-20 23:01:10 +0200

    changed ordering for projects

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -11,7 +11,7 @@ interface iAppPorps {
 export async function loader({}: LoaderArgs) {
   const query = gql`
     query MyQuery {
-      proejcts {
+      proejcts(orderBy: createdAt_ASC) {
         id
         link
         overview
